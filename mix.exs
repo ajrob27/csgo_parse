8d9c44c39f2b948a882c6750eb32e78f7c660fc2
AJ Robertson <aj@clearsightstudio.com>
2015-10-29 21:38:41 -0700

    Adds protobuf dependency

--- mix.exs
+++ mix.exs
@@ -17,16 +17,7 @@ defmodule CsgoParse.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:exprotobuf, "~> 0.12.0"}]
   end
 end
